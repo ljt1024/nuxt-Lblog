47f824cfc6ea78171858daa4fb3191464fd41d39
ljt1024 <1652009833@qq.com>
2022-08-09 23:10:45 +0800

    fix:完善聊天

--- pages/chart/index.vue
+++ pages/chart/index.vue
@@ -3,55 +3,71 @@
     <PageHead :active="4"/>
     <div class="page-main">
        <div class="chart-wrap">
-         <div class="chart-left chart-info">
-           <img src="../../static/images/logo.png" alt="" class="user-avatar">
-           <div class="chart-content-wrap">
-              <div class="username">111</div>
-              <div class="chart-content">122221312312312321312</div>
-           </div>
+         <div :class="item.sendId === userInfo.id ?['chart-right', 'chart-info'] : ['chart-left', 'chart-info']" v-for="(item, index) in chartList" :key="item.id">
+           <template v-if="item.sendId === userInfo.id">
+             <div class="chart-content-wrap">
+               <div class="username">{{item.username}}</div>
+               <div class="chart-content">{{item.content}}</div>
+             </div>
+             <img src="../../static/images/logo.png" alt="" class="user-avatar">
+           </template>
+           <template v-else>
+             <img src="../../static/images/logo.png" alt="" class="user-avatar">
+             <div class="chart-content-wrap">
+               <div class="username">{{item.username}}</div>
+               <div class="chart-content">{{item.content}}</div>
+             </div>
+           </template>
          </div>
-         <div class="chart-right chart-info">
-           <div class="chart-content-wrap">
-             <div class="username">ljt</div>
-             <div class="chart-content">44224</div>
-           </div>
-           <img src="../../static/images/logo.png" alt="" class="user-avatar">
-         </div>
-         <div class="chart-left chart-info">
-           <img src="../../static/images/logo.png" alt="" class="user-avatar">
-           <div class="chart-content-wrap">
-             <div class="username">111</div>
-             <div class="chart-content">122221312312312321312</div>
-           </div>
-         </div>
-         <div class="chart-left chart-info">
-           <img src="../../static/images/logo.png" alt="" class="user-avatar">
-           <div class="chart-content-wrap">
-             <div class="username">111</div>
-             <div class="chart-content">122221312312312321312</div>
-           </div>
-         </div>
-         <div class="chart-left chart-info">
-           <img src="../../static/images/logo.png" alt="" class="user-avatar">
-           <div class="chart-content-wrap">
-             <div class="username">111</div>
-             <div class="chart-content">122221312312312321312</div>
-           </div>
-         </div>
-         <div class="chart-left chart-info">
-           <img src="../../static/images/logo.png" alt="" class="user-avatar">
-           <div class="chart-content-wrap">
-             <div class="username">111</div>
-             <div class="chart-content">122221312312312321312</div>
-           </div>
-         </div>
-         <div class="chart-right chart-info">
-           <div class="chart-content-wrap">
-             <div class="username">ljt</div>
-             <div class="chart-content">44224</div>
-           </div>
-           <img src="../../static/images/logo.png" alt="" class="user-avatar">
-         </div>
+<!--        <div class="chart-left chart-info">-->
+<!--          <img src="../../static/images/logo.png" alt="" class="user-avatar">-->
+<!--          <div class="chart-content-wrap">-->
+<!--            <div class="username">111</div>-->
+<!--            <div class="chart-content">122221312312312321312</div>-->
+<!--          </div>-->
+<!--        </div>-->
+         <!--         <div class="chart-right chart-info">-->
+<!--           <div class="chart-content-wrap">-->
+<!--             <div class="username">ljt</div>-->
+<!--             <div class="chart-content">44224</div>-->
+<!--           </div>-->
+<!--           <img src="../../static/images/logo.png" alt="" class="user-avatar">-->
+<!--         </div>-->
+<!--         <div class="chart-left chart-info">-->
+<!--           <img src="../../static/images/logo.png" alt="" class="user-avatar">-->
+<!--           <div class="chart-content-wrap">-->
+<!--             <div class="username">111</div>-->
+<!--             <div class="chart-content">122221312312312321312</div>-->
+<!--           </div>-->
+<!--         </div>-->
+<!--         <div class="chart-left chart-info">-->
+<!--           <img src="../../static/images/logo.png" alt="" class="user-avatar">-->
+<!--           <div class="chart-content-wrap">-->
+<!--             <div class="username">111</div>-->
+<!--             <div class="chart-content">122221312312312321312</div>-->
+<!--           </div>-->
+<!--         </div>-->
+<!--         <div class="chart-left chart-info">-->
+<!--           <img src="../../static/images/logo.png" alt="" class="user-avatar">-->
+<!--           <div class="chart-content-wrap">-->
+<!--             <div class="username">111</div>-->
+<!--             <div class="chart-content">122221312312312321312</div>-->
+<!--           </div>-->
+<!--         </div>-->
+<!--         <div class="chart-left chart-info">-->
+<!--           <img src="../../static/images/logo.png" alt="" class="user-avatar">-->
+<!--           <div class="chart-content-wrap">-->
+<!--             <div class="username">111</div>-->
+<!--             <div class="chart-content">122221312312312321312</div>-->
+<!--           </div>-->
+<!--         </div>-->
+<!--         <div class="chart-right chart-info">-->
+<!--           <div class="chart-content-wrap">-->
+<!--             <div class="username">ljt</div>-->
+<!--             <div class="chart-content">44224</div>-->
+<!--           </div>-->
+<!--           <img src="../../static/images/logo.png" alt="" class="user-avatar">-->
+<!--         </div>-->
        </div>
       <div class="chart-input">
         <el-input
@@ -60,7 +76,8 @@
           resize="none"
           placeholder="请输入内容"
           :autosize="{ minRows: 4, maxRows: 4}"
-          v-model="content">
+          v-model="content"
+          @keyup.enter.native="submit">
         </el-input>
         <el-button type="primary" class="submit" @click="submit">发送</el-button>
       </div>
@@ -78,27 +95,49 @@
     data() {
       return {
         content: '',
-        socket: null
+        socket: null,
+        chartList: [],
+        userInfo: null
       }
     },
     mounted() {
+     this.userInfo = JSON.parse(localStorage.getItem('userInfo'))
+     this.getChartList()
      this.scrollBtm()
       this.socket = new WebSocket("ws://localhost:3002/socketTest");
-      this.socket.addEventListener("open", function (event) {
-        console.log("socket is open");
-        this.socket.send("这里是html发送过来的");
+      this.socket.addEventListener("open", (event)=> {
+        console.log("socket is open")
+        // this.socket.send("前端发送消息")
       });
-      this.socket.addEventListener("message", function (event) {
-        console.log("Message from server", event.data);
+      this.socket.addEventListener("message",  (event)=> {
+        console.log("接收到服务端发送的消息了")
+        if (JSON.parse(event.data).code === 200) {
+          console.log(1111);
+          this.getChartList()
+        }
       });
     },
     methods: {
       scrollBtm() {
         const chartWrap = document.querySelector('.chart-wrap')
-        chartWrap.scrollTop = chartWrap.scrollHeight;
+        chartWrap.scrollTop = chartWrap.scrollHeight
       },
       submit() {
-        this.socket.send(this.content);
+        let msg = {
+          sendId: this.userInfo.id,
+          avatar: this.userInfo.avatar,
+          username: this.userInfo.username,
+          content: this.content,
+          type: 1
+        }
+        this.socket.send(JSON.stringify(msg))
+      },
+      async getChartList() {
+        const result = await this.$axios.$get('/api/chartList')
+        this.chartList = result.data
+        setTimeout(()=> {
+          this.scrollBtm()
+        })
       }
     }
   }
@@ -127,7 +166,7 @@
         align-items: center;
       }
       .chart-left {
-        margin-bottom: 20px;
+        margin-bottom: 30px;
         .chart-content-wrap {
           margin-left: 10px;
         }
@@ -136,18 +175,19 @@
           padding: 6px 8px;
           border-radius: 10px;
           position: relative;
-          background-color: #fff;
+          color: var(--theme-bg-3);
+          background-color: var(--color-font-7);
           text-align: center;
           line-height: 25px;
           display: inline-block;
-          color: #333;
+          /*color: #333;*/
           font-size: 15px;
         }
         .chart-content::after {
           content: "";
           position: absolute;
           border: 5px solid transparent;
-          border-right-color:#fff;
+          border-right-color: var(--color-font-7);
           top: 30%;
           left: -10px;
         }
@@ -156,6 +196,7 @@
       .chart-right {
         display: flex;
         align-self: flex-end;
+        margin-bottom: 30px;
         .chart-content-wrap {
           margin-right: 10px;
         }
